dfc33361ffb0ff27bc2a0306175baedf22e8782d
Jing Conan Wang <hbhzwj@gmail.com>
2016-06-12 01:30:30 -0700

    Fix jshint error

--- tasks/ec2_assign_existing_address.js
+++ tasks/ec2_assign_existing_address.js
@@ -26,14 +26,15 @@ module.exports = function (grunt) {
 
         aws.log('ec2 associate-address --instance-id %s --public-ip %s', id, ip);
         var waitForParams = {
-          InstanceIds:  [id]
+            InstanceIds:  [id]
         };
         aws.ec2.waitFor('instanceRunning', waitForParams, function(err, data) {
-          if (err) {
-              console.log(err, err.stack); // an error occurred
-              return;
-          }
-          aws.ec2.associateAddress(params, aws.capture('Instance associated with public IP.', done));
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+                return;
+            }
+            console.log(data); // succesful response
+            aws.ec2.associateAddress(params, aws.capture('Instance associated with public IP.', done));
         });
     });
 };
